114065a3483bd5d4fb13d9ed2b146478713db968
Lucas S <lucas.santos.silva2011@gmail.com>
2022-11-08 19:23:03 -0300

    Adicionado mensagem de saudação

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Olá, " + nome);
     }
 }
